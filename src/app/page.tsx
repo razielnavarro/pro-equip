2cc883839b06d1cebe4d3e0ba094da8584be6884
Raziel Navarro <elrazand@gmail.com>
2025-03-16 21:55:42 -0500

    feat: add contact button to hero section for improved user engagement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,14 +17,19 @@ export default function Home() {
       <main>
         <Navbar />
         {/* Section with name and role */}
-        <div className="flex flex-col-reverse items-center md:flex-row md:justify-center min-h-[75vh] bg-[url(/assets/heroTest4.png)] bg-cover bg-center opacity 25">
-          <section className="gap-y-2 text-center md:text-left px-10 mb-12 w-full ">
-            <h1 className="text-4xl md:text-5xl text-left max-w-[800px] pl-50 text-white font-bold">
-              Energizando Panamá con Soluciones Eléctricas Confiables
-            </h1>
-            <p></p>
+        <div className="flex flex-col-reverse items-center md:flex-row md:justify-center min-h-[75vh] bg-[url(/assets/heroTest4.png)] bg-cover bg-center">
+          <section className="gap-y-2 text-center md:text-left px-10 mb-12 w-full">
+            <div className="max-w-[800px] text-left pl-50">
+              <h1 className="text-4xl md:text-5xl text-white font-bold">
+                Energizando Panamá con Soluciones Eléctricas Confiables
+              </h1>
+              <button className="mt-4 px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white font-semibold rounded-lg shadow-lg transition duration-300">
+                Contáctanos
+              </button>
+            </div>
           </section>
         </div>
+
         {/* Section with about us */}
         <div id="nosotros">
           <AboutUs />
