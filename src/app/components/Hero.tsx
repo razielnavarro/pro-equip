eee03fd2a7e6f5d9e15019d290089451e68951fc
Raziel Navarro <elrazand@gmail.com>
2025-03-25 14:44:56 -0500

    style: adjust diamond image positioning in HeroSection for improved layout

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,9 +1,9 @@
 const HeroSection = () => {
   return (
-    <div className="relative min-h-[550px] h-[50vh] lg:h-screen w-full overflow-visible z-20 flex justify-center items-center lg:items-start">
+    <div className="relative min-h-[600px] h-[50vh] xl:h-screen w-full overflow-visible z-20 flex justify-center items-center xl:items-start">
       {/* Background image hero */}
-      <div className="flex flex-col-reverse items-center lg:flex-row lg:justify-center h-full w-full bg-[url('/assets/FullBg.png')] bg-no-repeat bg-cover bg-center">
-        <section className="gap-y-2 text-center lg:text-left px-10 mb-12 w-full flex items-center justify-center lg:justify-start">
+      <div className="flex flex-col-reverse items-center xl:flex-row xl:justify-center h-full w-full bg-[url('/assets/FullBg.png')] bg-no-repeat bg-cover bg-center">
+        <section className="gap-y-2 text-center xl:text-left px-10 mb-12 w-full flex items-center justify-center xl:justify-start">
           <div className="max-w-[800px]">
             <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-3xl xl:text-4xl 2xl:text-5xl text-white font-bold">
               Energizando Panamá con Soluciones Eléctricas Confiables
@@ -17,7 +17,7 @@ const HeroSection = () => {
       </div>
 
       {/* Diamond/Polygon Image on top */}
-      <div className="absolute top-[10%] sm:top-[2%] md:top-[12%] lg:top-[54%] left-1/2 -translate-x-1/2 md:left-1/2 md:-translate-x-1/2 lg:left-auto lg:right-10 lg:translate-x-0 lg:transform lg:-translate-y-1/2 z-15 w-[300px] sm:w-[340px] md:w-[500px] lg:w-auto">
+      <div className="absolute top-[15%] sm:top-[10%] md:top-[6%] lg:top-[8%] xl:top-[50%] left-1/2 -translate-x-1/2 md:left-1/2 md:-translate-x-1/2 xl:left-auto xl:right-10 xl:translate-x-0 xl:transform xl:-translate-y-1/2 z-15 w-[300px] sm:w-[340px] md:w-[400px] xl:w-auto">
         <img
           src="/assets/diamondHigh.png"
           alt="Worker"
