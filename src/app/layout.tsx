afd7e94d0a1e886b80e60a7e914a1ddb71843d0e
Raziel Navarro <elrazand@gmail.com>
2025-03-25 21:05:39 -0500

    fix: remove tailwindcss-debug-screens plugin and clean up layout body class

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,9 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${defaultFont.className} antialiased relative debug-screens`}
-      >
+      <body className={`${defaultFont.className} antialiased relative`}>
         {children}
       </body>
     </html>
