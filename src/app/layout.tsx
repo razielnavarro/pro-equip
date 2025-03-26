9ae327a6d36756b91009473916219d0d1f796830
Raziel Navarro <elrazand@gmail.com>
2025-03-25 21:13:10 -0500

    feat: update site metadata and favicon for Proyectos y Equipos Eléctricos, S.A.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,8 +8,17 @@ const defaultFont = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Raziel Portfolio",
-  description: "Raziel Navarro personal portfolio",
+  title: "PROEQUIP, S.A.",
+  description:
+    "Proyectos y Equipos Eléctricos, S.A. es una empresa panameña con más de 25 años de experiencia en el diseño y construcción de proyectos eléctricos en media y baja tensión. Ofrecemos soluciones para urbanizaciones, proyectos industriales y comerciales, incluyendo instalaciones de transformadores, medición eléctrica, energía solar fotovoltaica y sistemas de puesta a tierra. Garantizamos eficiencia y sostenibilidad en cada uno de nuestros proyectos.",
+  keywords:
+    "diseño eléctrico, construcción de redes eléctricas, energía solar, instalación de transformadores, pruebas eléctricas, sistemas fotovoltaicos, potencia eléctrica, urbanizaciones, proyectos industriales, proyectos comerciales",
+  authors: [
+    {
+      name: "Proyectos y Equipos Eléctricos, S.A.",
+      url: "https://www.https://pro-equip.vercel.app/", //
+    },
+  ],
 };
 
 export default function RootLayout({
@@ -18,7 +27,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="es">
       <body className={`${defaultFont.className} antialiased relative`}>
         {children}
       </body>
